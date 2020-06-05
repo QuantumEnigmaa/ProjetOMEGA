bda57dd59c9c94e3254ff224b0508fef2ff8b9c9
lucchermette <lucchermette@mbp-de-luc>
2020-06-05 15:47:07 +0200

    etoiles

--- public_html/js/form.js
+++ public_html/js/form.js
@@ -7,24 +7,30 @@ and open the template in the editor.
     Created on : 4 juin 2020, 08:54:26
     Author     : Thibaud VAISSEAU
 */
-
-function validEnvoi() {
-    if (window.document.querySelector("validationCustom01").value === "" && 
-       window.document.querySelector("validationCustom02").value === "") {
-       alert("Le nom ou le prénom doivent être remplis"); //On affiche un message
+function validEnvoi() 
+{
+    if (window.document.querySelector("#nom").value === "" && 
+            window.document.querySelector("#prenom").value === "") 
+    {
+        alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
     }
-    else if (window.document.querySelector("validationCustom03").value === "") {
-        alert("L'email doit être rempli");
+    else if (window.document.querySelector("#mail").value === "") 
+    {
+        alert("L'email doit être rempli"); // On affiche un message
     }
-    else {
-        let question ="Souhaitez-vous réellement utiliser l'adresse suivante : "
-        +window.document.querySelector("validationCustom03l").value;
-        if(confirm(question)){
-            window.document.querySelector(".needs-validation").submit();// On peut envoyer
+    else
+    {
+        let question = "Souhaitez-vous réellement utiliser l'adresse suivante : " 
+                + window.document.querySelector("#mail").value;
+        if (confirm(question)) 
+        {
+            window.document.querySelector("#form_contact").submit(); // On peut envoyer
         }
     }
 }
 
-window.addEventListener("load",function() {
-window.document.querySelector(".btn btn-primary").addEventListener("click",validEnvoi);
+window.addEventListener("load", function() 
+{
+    window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
 });
+
